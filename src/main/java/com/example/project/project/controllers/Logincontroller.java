777ea24abec256ed780625ed82a0c3c36fe2ccdb
khaled <khaledamrahmed0@gmail.com>
2023-05-13 14:19:55 +0300

    sprint 1 second update

--- src/main/java/com/example/project/project/controllers/Logincontroller.java
+++ src/main/java/com/example/project/project/controllers/Logincontroller.java
@@ -3,13 +3,15 @@ package com.example.project.project.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.example.project.project.repositories.UserRepositories;
 
 @Controller
-@RequestMapping("/login")
+@RequestMapping("")
 public class Logincontroller {
 
     @Autowired
@@ -18,8 +20,19 @@ public class Logincontroller {
     @Autowired
     private UserRepositories userRepositories;
 
+    @PostMapping("/home")
+    public ModelAndView login(){
+        ModelAndView mav= new ModelAndView("home.html");
+        return mav;
 
-    
+    }
+
+    @GetMapping("/home")
+    public ModelAndView loginG(){
+        ModelAndView mav= new ModelAndView("home.html");
+        return mav;
+
+    }
 
     
     
