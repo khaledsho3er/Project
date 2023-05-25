648f3abc7b13ea9caf7c0e585eae55c029cdc4dc
martinaraafat <106750148+martinaraafat@users.noreply.github.com>
2023-05-26 01:42:11 +0300

    .

---
+++ src/main/java/com/example/project/project/controllers/userprofileController.java
@@ -0,0 +1,49 @@
+package com.example.project.project.controllers;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.example.project.project.Models.Flights;
+import com.example.project.project.Models.Reservation;
+import com.example.project.project.Models.User;
+import com.example.project.project.repositories.ReservationRepositiory;
+import com.example.project.project.repositories.FlightRepositories;
+
+@Controller
+public class userprofileController {
+    @Autowired
+    private FlightRepositories FlightRepositories;
+    @Autowired
+    private ReservationRepositiory reservationRepositiory;
+    
+    @GetMapping("/userprofile")
+    public ModelAndView profileform(@AuthenticationPrincipal User user){
+
+        ModelAndView mav = new ModelAndView("userprofile.html");
+
+       List <Reservation> resv = reservationRepositiory.findByUser(user);
+       mav.addObject("reservations", resv);
+
+
+        return mav;
+    }
+    // @PostMapping("/userprofile")
+    // public String savereserv(@ModelAttribute Flights flight, @AuthenticationPrincipal User user,
+    //         @RequestParam(name = "flightId", required = true) String flightId) {
+
+    //     Flights flight = this.FlightRepositories.findById(flightId).orElse(null);
+
+    //     reserv.setUser(user);
+    //     reserv.setFlights(flight);
+    //     this.reservationRepositiory.save(reserv);
+    //     return "redirect:/Payment?reservationId=" + reserv.getId();
+    // }
+
+}
--- src/main/java/com/example/project/project/repositories/ReservationRepositiory.java
+++ src/main/java/com/example/project/project/repositories/ReservationRepositiory.java
@@ -1,11 +1,18 @@
 package com.example.project.project.repositories;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.example.project.project.Models.Flights;
 import com.example.project.project.Models.Reservation;
+import com.example.project.project.Models.User;
 
 @Repository
 public interface ReservationRepositiory extends JpaRepository<Reservation, String> {
-
+    public Optional<Reservation> findById(String id);
+    public Optional<Reservation> findByFlights(Flights flights);
+    public List<Reservation> findByUser(User user);
 }
